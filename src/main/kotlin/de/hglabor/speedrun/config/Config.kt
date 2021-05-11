01912ad1497d6ab6706ef55e53db2728bb209f0d
Krxwallo <ken.grill@gmx.de>
2021-05-11 13:39:53 +0200

    Fixed config only gets default value

--- src/main/kotlin/de/hglabor/speedrun/config/Config.kt
+++ src/main/kotlin/de/hglabor/speedrun/config/Config.kt
@@ -7,7 +7,7 @@ import org.bukkit.ChatColor
 val PREFIX: String = "${ChatColor.DARK_GRAY}[${ChatColor.AQUA}Speedrun${ChatColor.DARK_GRAY}]${ChatColor.WHITE}"
 val MAX_PLAYERS: Int = Bukkit.getServer().maxPlayers
 
-enum class Config(private val path: String, private var value: Any) {
+enum class Config(private val path: String, value: Any) {
     MIN_PLAYERS("minPlayers", 4),
     CRAFTING_ROUNDS("craftingRounds", 5),
     CRAFTING_PREP_TIME("craftingPreparation", 5),
@@ -15,22 +15,23 @@ enum class Config(private val path: String, private var value: Any) {
     RESTART_TIME("restartAfter", 30),
     DO_RESTART("doRestart", true);
 
+    private val mValue: Any get() = PLUGIN.config.get(this.path)!!;
+
     companion object {
         fun load() {
-            values().forEach { PLUGIN.config.addDefault(it.path, it.value) }
+            values().forEach { PLUGIN.config.addDefault(it.path, it.mValue) }
             PLUGIN.config.options().copyDefaults(true)
             PLUGIN.saveConfig()
         }
     }
 
     fun set(value: Int) {
-        this.value = value
-        PLUGIN.config.set(this.path, this.value)
+        PLUGIN.config.set(this.path, value)
         PLUGIN.saveConfig()
     }
 
-    fun getInt(): Int = this.value as Int
-    fun getBoolean(): Boolean = this.value as Boolean
+    fun getInt(): Int = this.mValue as Int
+    fun getBoolean(): Boolean = this.mValue as Boolean
 
     fun reload() { PLUGIN.reloadConfig() }
 }
