262df8a99752cf39e07ac27a031364d71ff31dcd
Krxwallo <ken.grill@gmx.de>
2021-06-09 10:51:14 +0200

    Fix can destroy entities

--- src/main/kotlin/de/hglabor/speedrun/listener/MainListener.kt
+++ src/main/kotlin/de/hglabor/speedrun/listener/MainListener.kt
@@ -6,13 +6,11 @@ import de.hglabor.speedrun.utils.cancel
 import de.hglabor.speedrun.utils.isCreative
 import net.axay.kspigot.event.listen
 import net.axay.kspigot.extensions.bukkit.feed
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.Player
+import org.bukkit.entity.*
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
 import org.bukkit.event.entity.*
-import org.bukkit.event.player.PlayerDropItemEvent
-import org.bukkit.event.player.PlayerMoveEvent
+import org.bukkit.event.player.*
 
 fun mainListener() {
     listen<FoodLevelChangeEvent> {
@@ -35,6 +33,12 @@ fun mainListener() {
     }
     listen<PlayerMoveEvent> {
         if (it.player.location.y <= 10 && GamePhaseManager.currentState != GameState.Stronghold) it.player.teleport(it.player.world.spawnLocation)
+    }
+    listen<PlayerInteractAtEntityEvent> {
+        if (!it.player.isCreative() && it.rightClicked is ArmorStand) it.cancel()
+    }
+    listen<EntityDamageByEntityEvent> {
+        if (it.damager is Player && !it.damager.isCreative()) it.cancel()
     }
 
     listen<EntitySpawnEvent> {
